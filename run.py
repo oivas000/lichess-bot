f3ec5e2f1df725261ead35ec9dfc7f8c9ef46b62
Savioprince <69968870+OIVAS7572@users.noreply.github.com>
2021-02-07 19:32:34 +0530

    Update run.py

--- run.py
+++ run.py
@@ -2,6 +2,6 @@ import os
 
 def lichess():
     program = "python3"
-    arguments = ["lichess-bot.py -u"]
+    arguments = ["lichess-bot.py","-u"]
     print(os.execvp(program, (program,) +  tuple(arguments)))
 lichess()    
