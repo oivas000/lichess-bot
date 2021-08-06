b062b3f4ef45e94a4c225c458591dce754016884
barca-cloud <85890899+barca-cloud@users.noreply.github.com>
2021-08-06 20:40:02 +0530

    Update conversation.py

--- conversation.py
+++ conversation.py
@@ -45,12 +45,14 @@ class Conversation:
 
     def send_reply(self, line, reply):
         self.xhr.chat(self.game.id, line.room, reply)
+        
+    def send_message(self, room, message):
+        if message:
+            self.send_reply(ChatLine({"room": room}), message)
 
 
 class ChatLine:
-    class ChatLine:
     def __init__(self, json):
         self.room = json.get("room")
         self.username = json.get("username")
         self.text = json.get("text")
-
