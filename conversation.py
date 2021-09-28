00c73368aa82633261752a080b7d3ea86a41677d
SAVIO PRINCE <69968870+OIVAS7572@users.noreply.github.com>
2021-09-28 08:55:06 +0000

    corrected format

--- conversation.py
+++ conversation.py
@@ -30,7 +30,7 @@ class Conversation:
         elif cmd == "id":
             self.send_reply(line, "OIVAS7572")
         elif cmd == "howto":
-           self.send_reply(line, "How to run: Check out 'Lichess Bot API'")
+            self.send_reply(line, "How to run: Check out 'Lichess Bot API'")
         elif cmd == "eval" and line.room == "spectator":
             stats = self.engine.get_stats(board, for_chat=True)
             self.send_reply(line, ", ".join(stats))
